ce5ecfe80400437a4c612c4754bfbf2b0d18dad5
alagache <alagache@student.42.fr>
2023-04-12 22:10:56 +0200

    add docker kubectl k3d install for jammy in p3

---
+++ p3a/scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Docker install
+sudo apt-get update
+sudo apt install -y ca-certificates curl apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+# systemctl status docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+#kubectl install
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#k3d install
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
