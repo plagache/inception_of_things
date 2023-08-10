3812ec5284aae743dbf81127f4819b279ef98f3c
alagache <alagache@student.42.fr>
2023-08-10 17:40:55 +0200

    TODO split scripts
    need docker groups to use docker without sudo

--- p3a/scripts/install.sh
+++ p3a/scripts/install.sh
@@ -32,3 +32,5 @@ rm argocd-linux-amd64
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl port-forward svc/argocd-server -n argocd 8080:443
+#https://argo-cd.readthedocs.io/en/stable/getting_started/#4-login-using-the-cli
