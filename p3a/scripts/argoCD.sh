9d94224d12313ab5595dbd700e96bd77c1b0b670
alagache <alagache@student.42.fr>
2023-08-13 18:06:29 +0000

    prints better

--- p3a/scripts/argoCD.sh
+++ p3a/scripts/argoCD.sh
@@ -6,10 +6,10 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 argocd admin initial-password -n argocd &> /dev/null
 while [ "$?" -ne 0 ]; do printf "\rwaiting for argocd to boot up";sleep 2; argocd admin initial-password -n argocd &> /dev/null; done
 
-printf '\n'
+printf '\nPassword:\n'
 argocd admin initial-password -n argocd
 
-printf '\nRun this command in another shell:\n\nkubectl port-forward svc/argocd-server -n argocd 8080:443\n\nto be able to login with:\nargocd login localhost:8080\n\n'
+printf '\nRun this command in another shell:\n\nkubectl port-forward svc/argocd-server -n argocd 8080:443\n\nto be able to login with:\n\nargocd login localhost:8080\n\n'
 
 #argocd app create coolapp --repo $REPOURL --path coolapp --dest-server https://kubernetes.default.svc --dest-namespace dev
 #argocd app get coolapp
