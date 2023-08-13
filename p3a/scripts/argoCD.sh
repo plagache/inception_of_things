bbfdff2253c0fc72e2924f5e9b9fb956c5c3ddb2
alagache <alagache@student.42.fr>
2023-08-13 18:01:25 +0000

    prints

--- p3a/scripts/argoCD.sh
+++ p3a/scripts/argoCD.sh
@@ -4,11 +4,11 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 argocd admin initial-password -n argocd &> /dev/null
-while [ "$?" -ne 0 ]; do sleep 2; argocd admin initial-password -n argocd &> /dev/null; done
+while [ "$?" -ne 0 ]; do printf "\rwaiting for argocd to boot up";sleep 2; argocd admin initial-password -n argocd &> /dev/null; done
 
 argocd admin initial-password -n argocd
 
-printf 'Run this command in another shell:\nkubectl port-forward svc/argocd-server -n argocd 8080:443\n to be able to login with:\nargocd login localhost:8080'
+printf '\nRun this command in another shell:\n\nkubectl port-forward svc/argocd-server -n argocd 8080:443\n\n to be able to login with:\nargocd login localhost:8080\n\n'
 
 #argocd app create coolapp --repo $REPOURL --path coolapp --dest-server https://kubernetes.default.svc --dest-namespace dev
 #argocd app get coolapp
