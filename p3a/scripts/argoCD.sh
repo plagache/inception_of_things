82cb1f4799345dfb2c49046f3767c8559c977fb4
alagache <alagache@student.42.fr>
2023-08-30 14:50:28 +0200

    add sleep to let changes occure before continuing

--- p3a/scripts/argoCD.sh
+++ p3a/scripts/argoCD.sh
@@ -10,9 +10,10 @@ while [ "$?" -ne 0 ]; do printf "\rwaiting for argocd to boot up $counter"; ((co
 
 
 PASSWORD=$(argocd admin initial-password -n argocd | head -n 1)
+printf "\nadmin password:\n$PASSWORD\n"
 LOCALPORT=24242
 CONTAINERPORT=443
 
-kubectl port-forward svc/argocd-server -n argocd $LOCALPORT:$CONTAINERPORT > /dev/null 2>&1 &
+nohup kubectl port-forward svc/argocd-server -n argocd $LOCALPORT:$CONTAINERPORT > /dev/null 2>&1 &
 
-argocd login localhost:$LOCALPORT --insecure --username admin --password $PASSWORD
+sleep 5 ;argocd login localhost:$LOCALPORT --insecure --username admin --password $PASSWORD; sleep 5
