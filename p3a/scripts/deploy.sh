f2cbc9a61bd5b22566497bd052b9ef4652db27c1
alagache <alagache@student.42.fr>
2023-08-30 09:41:46 +0000

    wait for pod to boot up before sending portforward

--- p3a/scripts/deploy.sh
+++ p3a/scripts/deploy.sh
@@ -5,6 +5,10 @@ argocd app create coolapp --repo $REPOURL --path $APPPATH --dest-server https://
 argocd app get coolapp
 argocd app sync coolapp
 
+printf "\nNOT runing yet\n"
+while [ "$?" -ne 0 ]; do printf "\rwaiting for $APPPATH to boot up $counter"; ((counter++));sleep 1; kubectl get pods -n dev | grep cool-app | grep Running; done
+
+printf "\nRuning\n"
 LOCALPORT=4242
 CONTAINERPORT=8888
 printf "\nRun this command in another shell:\n\nkubectl port-forward svc/cool-app -n dev $LOCALPORT:$CONTAINERPORT\n\nto be able to connect to the app with:\n\ncurl localhost:$LOCALPORT\n\n"
