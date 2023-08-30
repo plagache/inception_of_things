6ff3853191ed9772754896913dbc58033feb1399
alagache <alagache@student.42.fr>
2023-08-30 09:21:56 +0000

    add flags to login command

--- p3a/scripts/argoCD.sh
+++ p3a/scripts/argoCD.sh
@@ -8,9 +8,9 @@ counter=0
 argocd admin initial-password -n argocd &> /dev/null
 while [ "$?" -ne 0 ]; do printf "\rwaiting for argocd to boot up $counter"; ((counter++));sleep 1; argocd admin initial-password -n argocd &> /dev/null; done
 
-printf '\nPassword:\n'
-argocd admin initial-password -n argocd
 
+PASSWORD=$(argocd admin initial-password -n argocd)
+printf "\nadmin password:\n$PASSWORD\n"
 LOCALPORT=24242
 CONTAINERPORT=443
-printf "\nRun this command in another shell:\n\nkubectl port-forward svc/argocd-server -n argocd $LOCALPORT:$CONTAINERPORT\n\nto be able to login with:\n\nargocd login localhost:$LOCALPORT\n\n"
+printf "\nRun this command in another shell:\n\nkubectl port-forward svc/argocd-server -n argocd $LOCALPORT:$CONTAINERPORT\n\nto be able to login with:\n\nargocd login localhost:$LOCALPORT --insecure --user admin --password $PASSWORD\n\n"
