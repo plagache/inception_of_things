fd42d1262b5452433b6959bedea032de5f664be1
plgch <plagache@protonmail.com>
2023-12-04 11:20:00 +0100

    move checking in check;

--- p3a/scripts/deploy.sh
+++ p3a/scripts/deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
+
+
 REPOURL="https://gitlab.com/3rdn4x3l4/deploy-svc-iot.git"
 APPPATH="coolapp"
 argocd app create coolapp --repo $REPOURL --path $APPPATH --dest-server https://kubernetes.default.svc --dest-namespace dev
 argocd app get coolapp
-argocd app sync coolapp
-
-false
-while [ "$?" -ne 0 ]; do printf "\rwaiting for $APPPATH to boot up $counter"; ((counter++));sleep 1; kubectl get pods -n dev | grep cool-app | grep Running &> /dev/null; done
-
-printf "\n$APPPATH Runing\n"
